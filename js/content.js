34889512dd30e0aecfd0b7106a150e2d691bad5c
szikonyaattila <47735973+szikonyaattila@users.noreply.github.com>
2019-02-25 00:11:52 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -4,20 +4,28 @@ class Content {
     content(req, res) {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
         
+        res.write("<h1>Egy változó létre hozása var-al és változtatásával</h1>");
         var a = 31;
-        res.write("<p>decimal érték: " + a + "</p>");
+        res.write("<p>A változó értéke: " + a + "</p>");
+        if(true)
+        {
+            var a = 33;
+            res.write("<p>A változó értéke amit egy if-ben módosítunk: " + a + "</p>");
+        }
+        res.write("<p>A változó értéke az if után: " + a + "</p>");
         
+        res.write("<h1>Egy változó létre hozása let-tel és változtatásával</h1>");
         let b =32;
-        res.write("<p>decimal érték: " + b + "</p>");
+        res.write("<p>A változó értéke: " + b + "</p>");
         if(true)
         {
             let b = 33;
-            res.write("<p>decimal érték: " + b + "</p>");
+            res.write("<p>A változó értéke amit egy if-ben módosítunk: " + b + "</p>");
         }
-        res.write("<p>decimal érték: " + b + "</p>");
+        res.write("<p>A változó értéke if után: " + b + "</p>");
         
         res.write("<a href='https://github.com/szikonyaattila/letvsvar/' target='_blank'>" +
-            "https://github.com/szikonyaattila/letvsvar//</a><br>");
+            "https://github.com/szikonyaattila/letvsvar/</a><br>");
         res.end();
     }
 }
