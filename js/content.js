f1fadcdd5aa9fce74fc53e22987ea0d2f1a92f5c
szikonyaattila <47735973+szikonyaattila@users.noreply.github.com>
2019-02-25 00:01:10 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,21 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Content {
     content(req, res) {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        var valtozo1 = 31;
-        res.write("<p>decimal érték: " + valtozo1 + "</p>");
+        
+        var a = 31;
+        res.write("<p>decimal érték: " + a + "</p>");
          if (true) {
-                var valtozo1 = 71;   
-                res.write("<p>decimal érték: " + valtozo1 + "</p>");
+                 a = 71;   
+                res.write("<p>decimal érték: " + a + "</p>");
             }
         
-        let valtozo2 =32;
-        res.write("<p>decimal érték: " + valtozo2 + "</p>");
+        let b =32;
+        res.write("<p>decimal érték: " + b + "</p>");
         if (true) {
-                var valtozo2 = 72;   
-                res.write("<p>decimal érték: " + valtozo2 + "</p>");
+                 b = 72;   
+                res.write("<p>decimal érték: " + b + "</p>");
             }
-        res.write("<p>decimal érték: " + valtozo1 + "</p>");
-        res.write("<p>hex érték: " + valtozo2 + "</p>");
+        res.write("<p>decimal érték: " + a + "</p>");
+        res.write("<p>hex érték: " + b + "</p>");
         
         res.write("<a href='https://github.com/szikonyaattila/letvsvar/' target='_blank'>" +
             "https://github.com/szikonyaattila/letvsvar//</a><br>");
