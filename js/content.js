1fd565d88aed8e824694557010b23204042d7d81
szikonyaattila <47735973+szikonyaattila@users.noreply.github.com>
2019-02-24 23:37:09 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -2,31 +2,31 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 class Content {
     content(req, res) {
+        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
         function varTest() {
             var x = 31;
             if (true) {
                 var x = 71; 
                 alert(x);  
-                res.write("<p>"+ x +"</p>");
+                res.write("<p>hex érték: " + x + "</p>");
             }
-            alert(x);
-            res.write("<p>"+ x +"</p>");
+            alert(x); 
+            res.write("<p>hex érték: " + x + "</p>");
         }
         function letTest() {   
             let x = 31;   
             if (true) {   
                 let x = 71; 
-                alert(x);  
-                res.write("<p>"+ x +"</p>");   
+                alert(x); 
+                res.write("<p>hex érték: " + x + "</p>");   
             }   
-                alert(x);
-                res.write("<p>"+ x +"</p>");   
-        }    
+                alert(x); 
+                res.write("<p>hex érték: " + x + "</p>");   
+        }
         res.write("<a href='https://github.com/szikonyaattila/letvsvar/' target='_blank'>" +
-            "https://github.com/szikonyaattila/letvsvar/</a><br>");
+            "https://github.com/szikonyaattila/letvsvar//</a><br>");
         res.end();
     }
 }
 exports.default = Content;
 //# sourceMappingURL=content.js.map
-
