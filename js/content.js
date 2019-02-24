059be91fb30802ddd918f8903273158ab1824337
szikonyaattila <47735973+szikonyaattila@users.noreply.github.com>
2019-02-24 23:49:21 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -3,20 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class Content {
     content(req, res) {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        let a = 31;
-        let b = true;
-        if (a>1) 
-        {
-               let x = 71;   
-               res.write("<p>a érték:"+ x +"</p>");
-        }
+        let a = 31;   
         res.write("<p>a érték:"+ x +"</p>");
-        let a = 31;
-        if (a>1) 
-        {
-               let x = 71;   
-               res.write("<p>a érték:"+ x +"</p>");
-        }
+        let a = 31;        
         res.write("<p>a érték:"+ x +"</p>");
         
         res.write("<a href='https://github.com/szikonyaattila/letvsvar/' target='_blank'>" +
