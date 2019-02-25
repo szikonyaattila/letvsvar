38803d07e7f0f4a63be8080c202b0e4e6e52dfdf
szikonyaattila <47735973+szikonyaattila@users.noreply.github.com>
2019-02-25 09:24:28 +0100

    Update content.ts

--- ts/content.ts
+++ ts/content.ts
@@ -3,29 +3,15 @@ import * as http from "http";
 export default class Content {
 
     public content(req: http.IncomingMessage, res: http.ServerResponse): void {
+    for(var i =0 ; i<5 ; i++){
+       console.log(i) 
+    }
+    console.log(i) // i=5
 
-      
-        function varTest() {
-            var x = 31;
-            if (true) {
-                var x = 71;  // ugyanaz a változó!
-                alert(x);  // 71
-                res.write("<p>"+ x +"</p>");
-            }
-            alert(x);  // 71
-            res.write("<p>"+ x +"</p>");
-        }
-        function letTest() {   
-            let x = 31;   
-            if (true) {   
-                let x = 71;  // másik változó   
-                alert(x); // 71 
-                res.write("<p>"+ x +"</p>");   
-            }   
-                alert(x);  // 31
-                res.write("<p>"+ x +"</p>");   
-        }
-
+    for(let i = 0; i<5;i++){
+       console.log(i)
+    }
+    console.log(i)
         
         res.write("<a href='https://github.com/szikonyaattila/letvsvar/' target='_blank'>" +
             "https://github.com/szikonyaattila/letvsvar/</a><br>");
